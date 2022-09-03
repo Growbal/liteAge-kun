6f96572d8b858207fc35e6ed52eeb27f542be01d
KessaPassa <k.someya.gyao@gmail.com>
2022-09-04 02:03:35 +0900

    add: vue3とvuetify3を導入した

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-app>
+    <v-main>
+      <router-view />
+      <h1>ほげ</h1>
+      <v-alert color="rgba(255, 0, 0, 0.5)" text="{{message}}"></v-alert>
+      <v-badge></v-badge>
+      <p>{{ message }}</p>
+      <h2 :class="numberClass">{{ number }} {{ double }}</h2>
+      <button @click="add(1)">+1</button> <button @click="add(10)">+10</button>
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+
+  data() {
+    return {
+      message: "Hello Vue3",
+      number: 12,
+      double: 3,
+    };
+  },
+  computed: {
+    double() {
+      return this.number * 2;
+    },
+    numberClass() {
+      if (this.number > 50) {
+        return "red";
+      }
+
+      return "blue";
+    },
+  },
+  methods: {
+    add(val: number) {
+      this.number = this.number + val;
+    },
+  },
+});
+</script>
---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import "vuetify/styles";
+import { createApp } from "vue";
+import router from "./router";
+import App from "./App.vue";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import { loadFonts } from "./plugins/webfontloader";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+loadFonts();
+
+createApp(App).use(router).use(vuetify).mount("#app");
