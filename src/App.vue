efd639c0570b9ada4040d0992d5ffb5aa0434149
Kazuki.Sakamoto <kaitoxtennis1997mail@gmail.com>
2022-09-05 15:20:31 +0900

    バグ修正とレイアウト修正

--- src/App.vue
+++ src/App.vue
@@ -2,158 +2,158 @@
   <v-app>
     <v-main>
       <router-view />
-      <MainHeader/>
-      <div>
+      <MainHeader />
+      <div class="execpannel">
         <v-row align="start" no-gutters style="height: 800px">
           <QuestionPannel />
-          <v-col cols="9">
-            <div class="d-flex justify-center pr-3">残り{{ timer }}秒</div>
-            <PhoneHeader/>
-            <LineTitle/>
-            <div>
-              <v-toolbar v-show="show">
-                <v-col>
-                  <v-row justify="end">
-                    <v-dialog v-model="lineTelDialog"  persistent width="2000">
-                      <template v-slot:activator="{ isActive: on, props: attrs }">
-                        <v-btn v-bind="attrs" v-on="on" class="ml-4" variant="text" icon="mdi-phone"></v-btn>
-                      </template>
-                      <v-card height="50vh" width="50vh">
-                        <v-card-text></v-card-text>
-                        <v-row justify="center">
-                          <v-avatar color="teal" size="56">
-                            <img src="./assets/keirou_obaachan.png" class="avatar-picture">
-                          </v-avatar>
-                        </v-row>
-                        <h4 class="text-center">おばあちゃん</h4>
-                        <v-card-text></v-card-text>
-                        <v-card-text></v-card-text>
-                        <v-card-text></v-card-text>
-                        <v-card-actions>
-                          <v-row>
-                            <v-col>
-                              <v-row justify="center">
-                                <v-btn variant="text" icon="mdi-microphone-outline"></v-btn>
-                              </v-row>
-                              <v-row justify="center">
-                                <p class="text-caption">マイクをオフ</p>
-                              </v-row>
-                            </v-col>
-                            <v-col>
-                              <v-row justify="center">
-                                <v-btn variant="text" icon="mdi-close-circle" @click="lineTelDialog = false"></v-btn>
-                              </v-row>
-                              <v-row justify="center">
-                                <p class="text-caption">ビデオを終了</p>
-                              </v-row>
-                            </v-col>
-                            <v-col>
-                              <v-row justify="center">
-                                <v-btn  variant="text" icon="mdi-volume-source"></v-btn>
-                              </v-row>
-                              <v-row justify="center">
-                                <p class="text-caption">スピーカー</p>
-                              </v-row>
-                            </v-col>
+            <v-col cols="9" >
+              <div class="d-flex justify-center pr-3">残り{{ timer }}秒</div>
+              <div class="linepannel">
+                <PhoneHeader />
+                <LineTitle v-on:click="click" />
+                <v-toolbar v-show="show">
+                  <v-col>
+                    <v-row justify="end">
+                      <v-dialog v-model="lineTelDialog" persistent width="2000">
+                        <template v-slot:activator="{ isActive: on, props: attrs }">
+                          <v-btn v-bind="attrs" v-on="on" class="ml-4" variant="text" icon="mdi-phone"></v-btn>
+                        </template>
+                        <v-card height="50vh" width="50vh">
+                          <v-card-text></v-card-text>
+                          <v-row justify="center">
+                            <v-avatar color="teal" size="56">
+                              <img src="./assets/keirou_obaachan.png" class="avatar-picture">
+                            </v-avatar>
                           </v-row>
-                        </v-card-actions>
-                        <v-card-text></v-card-text>
-                      </v-card>
-                    </v-dialog>
-                  </v-row>
-                  <v-row justify="end">
-                    <p class="text-caption">&nbsp;&nbsp;&nbsp;&nbsp;音声通話</p>
-                  </v-row>
-                </v-col>
-                <v-spacer></v-spacer>
-                <v-col>
-                  <v-row justify="start">
-                    <v-btn class="ml-4" variant="text" icon="mdi-video"></v-btn>
-                  </v-row>
-                  <v-row justify="start">
-                    <p class="text-caption">&nbsp;&nbsp;ビデオ通話</p>
-                  </v-row>
-                </v-col>
-              </v-toolbar>
-            </div>
-            <v-card  class="pa-2 ma-2" outlined tile color="black">
-              <div v-for="chat in chats">
-                <v-layout class="justify-start pb-3" v-if="!chat.isUser">
-                  <v-avatar color="teal" size="56">
-                    <img
-                      src="./assets/keirou_obaachan.png"
-                      class="avatar-picture"
-                    />
-                  </v-avatar>
-                  <v-card outlined color="rgb(239	239	239)" class="chat-message">
-                    <v-card-text>{{ chat.message }}</v-card-text>
-                  </v-card>
-                  <p class="text-caption pl-3 chat-capition">{{ nowTime }}</p>
-                </v-layout>
-                <v-layout class="justify-end pb-3" v-if="chat.isUser">
-                  <p class="text-caption pr-3 chat-capition">
+                          <h4 class="text-center">おばあちゃん</h4>
+                          <v-card-text></v-card-text>
+                          <v-card-text></v-card-text>
+                          <v-card-text></v-card-text>
+                          <v-card-actions>
+                            <v-row>
+                              <v-col>
+                                <v-row justify="center">
+                                  <v-btn variant="text" icon="mdi-microphone-outline"></v-btn>
+                                </v-row>
+                                <v-row justify="center">
+                                  <p class="text-caption">マイクをオフ</p>
+                                </v-row>
+                              </v-col>
+                              <v-col>
+                                <v-row justify="center">
+                                  <v-btn variant="text" icon="mdi-close-circle" @click="lineTelDialog = false"></v-btn>
+                                </v-row>
+                                <v-row justify="center">
+                                  <p class="text-caption">ビデオを終了</p>
+                                </v-row>
+                              </v-col>
+                              <v-col>
+                                <v-row justify="center">
+                                  <v-btn variant="text" icon="mdi-volume-source"></v-btn>
+                                </v-row>
+                                <v-row justify="center">
+                                  <p class="text-caption">スピーカー</p>
+                                </v-row>
+                              </v-col>
+                            </v-row>
+                          </v-card-actions>
+                          <v-card-text></v-card-text>
+                        </v-card>
+                      </v-dialog>
+                    </v-row>
+                    <v-row justify="end">
+                      <p class="text-caption">&nbsp;&nbsp;&nbsp;&nbsp;音声通話</p>
+                    </v-row>
+                  </v-col>
+                  <v-spacer></v-spacer>
+                  <v-col>
+                    <v-row justify="start">
+                      <v-btn class="ml-4" variant="text" icon="mdi-video"></v-btn>
+                    </v-row>
+                    <v-row justify="start">
+                      <p class="text-caption">&nbsp;&nbsp;ビデオ通話</p>
+                    </v-row>
+                  </v-col>
+                </v-toolbar>
+              <v-card class="pa-5" height="52vh" >
+                <div v-for="chat in chats">
+                  <v-layout class="justify-start pb-3" v-if="!chat.isUser">
+                    <v-avatar color="teal" size="56">
+                      <img src="./assets/keirou_obaachan.png" class="avatar-picture" />
+                    </v-avatar>
+                    <v-card outlined color="rgb(239	239	239)" class="chat-message">
+                      <v-card-text>{{ chat.message }}</v-card-text>
+                    </v-card>
+                    <p class="text-caption pl-3 chat-capition">{{ nowTime }}</p>
+                  </v-layout>
+                  <v-layout class="justify-end pb-3" v-if="chat.isUser">
+                    <p class="text-caption pr-3 chat-capition">
                     <p style="width: 100%">既読</p>
                     <p>{{ nowTime }}</p>
-                  </p>
-                  <v-card outlined color="rgb(134	217	123)" class="chat-message">
-                    <v-card-text>{{ chat.message }}</v-card-text>
-                  </v-card>
-                </v-layout>
-              </div>
-            </v-card>
-            <v-toolbar dark prominent src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg">
-              <v-icon icon="mdi-plus" class="ml-3"></v-icon>
-              <v-icon icon="mdi-camera" class="ml-3"></v-icon>
-              <v-icon icon="mdi-image" class="ml-3"></v-icon>
-              <v-form class="d-flex" style="flex-grow: 1; flex-shrink: 1" @submit.prevent="sendMessage">
-                <v-text-field v-model="chatMessage" placeholder="メッセージを入力してください"></v-text-field>
-              </v-form>
-              <v-icon icon="mdi-microphone" class="ml-3"></v-icon>
-              <v-app-bar-nav-icon></v-app-bar-nav-icon>
-            </v-toolbar>
-          </v-col>
+                    </p>
+                    <v-card outlined color="rgb(134	217	123)" class="chat-message">
+                      <v-card-text>{{ chat.message }}</v-card-text>
+                    </v-card>
+                  </v-layout>
+                </div>
+              </v-card>
+
+              <v-toolbar prominent color="rgb(0, 195, 0,0.5)">
+                <v-icon icon="mdi-plus" class="ml-3"></v-icon>
+                <v-icon icon="mdi-camera" class="ml-3"></v-icon>
+                <v-icon icon="mdi-image" class="ml-3"></v-icon>
+                <v-form class="d-flex" style="flex-grow: 1; flex-shrink: 1" @submit.prevent="sendMessage">
+                  <v-text-field v-model="chatMessage" placeholder="メッセージを入力してください"></v-text-field>
+                </v-form>
+                <v-icon icon="mdi-microphone" class="ml-3"></v-icon>
+                <v-app-bar-nav-icon></v-app-bar-nav-icon>
+              </v-toolbar>
+            </div>
+            </v-col>
         </v-row>
       </div>
 
       <v-dialog v-model="phoneTelDialog" persistent width="2000">
-      <template v-slot:activator="{ isActive: on, props: attrs }">
-        <v-btn v-bind="attrs" v-on="on" class="ml-4" variant="text" icon="mdi-phone"></v-btn>
-      </template>
-      <v-card height="50vh" width="70vh" color="black">
-        <v-card-text></v-card-text>
-        <v-row justify="center">
-          <v-avatar color="teal" size="56">
-            <img src="./assets/keirou_obaachan.png" class="avatar-picture">
-          </v-avatar>
-        </v-row>
-        <h4 class="text-center">おばあちゃん</h4>
-        <center><p class="text-caption">日本</p></center>
-        <v-card-text></v-card-text>
-        <v-card-text></v-card-text>
-        <v-card-actions>
-          <v-row>
-            <v-col>
-              <v-row justify="center">
-                <v-btn variant="text" icon="mdi-microphone-outline"></v-btn>
-              </v-row>
-              <v-row justify="center">
-                <p class="text-caption">あとで通知</p>
-              </v-row>
-            </v-col>
-            <v-col>
-              <v-row justify="center">
-                <v-btn variant="text" icon="mdi-close-circle" @click="phoneTelDialog = false"></v-btn>
-              </v-row>
-              <v-row justify="center">
-                <p class="text-caption">メッセージを送信</p>
-              </v-row>
-            </v-col>
+        <template v-slot:activator="{ isActive: on, props: attrs }">
+          <v-btn v-bind="attrs" v-on="on" class="ml-4" variant="text" icon="mdi-phone"></v-btn>
+        </template>
+        <v-card height="50vh" width="70vh" color="black">
+          <v-card-text></v-card-text>
+          <v-row justify="center">
+            <v-avatar color="teal" size="56">
+              <img src="./assets/keirou_obaachan.png" class="avatar-picture">
+            </v-avatar>
           </v-row>
-        </v-card-actions>
-        <v-btn variant="text" icon="mdi-close-circle" @click="phoneTelDialog = false"></v-btn>
-        <v-card-text></v-card-text>
-      </v-card>
-    </v-dialog>
+          <h4 class="text-center">おばあちゃん</h4>
+          <center>
+            <p class="text-caption">日本</p>
+          </center>
+          <v-card-text></v-card-text>
+          <v-card-text></v-card-text>
+          <v-card-actions>
+            <v-row>
+              <v-col>
+                <v-row justify="center">
+                  <v-btn variant="text" icon="mdi-microphone-outline"></v-btn>
+                </v-row>
+                <v-row justify="center">
+                  <p class="text-caption">あとで通知</p>
+                </v-row>
+              </v-col>
+              <v-col>
+                <v-row justify="center">
+                  <v-btn variant="text" icon="mdi-close-circle" @click="phoneTelDialog = false"></v-btn>
+                </v-row>
+                <v-row justify="center">
+                  <p class="text-caption">メッセージを送信</p>
+                </v-row>
+              </v-col>
+            </v-row>
+          </v-card-actions>
+          <v-btn variant="text" icon="mdi-close-circle" @click="phoneTelDialog = false"></v-btn>
+          <v-card-text></v-card-text>
+        </v-card>
+      </v-dialog>
     </v-main>
   </v-app>
 
@@ -177,7 +177,6 @@ interface Chat {
   message: string;
 }
 
-
 export default defineComponent({
   name: "App",
 
@@ -190,6 +189,7 @@ export default defineComponent({
       chats: [] as Chat[],
       chatMessage: "" as string,
       botMessageNumber: 0 as number,
+      
     };
   },
 
@@ -282,12 +282,21 @@ export default defineComponent({
   width: 48px;
   height: 48px;
 }
+
 .chat-message {
   max-width: 50%;
 }
+
 .chat-capition {
   display: flex;
   flex-direction: column;
   justify-content: flex-end;
+}
+
+.execpannel {
+  background-color: brown;
+}
+.linepannel{
+  background-color: white;
 }
 </style>
--- src/components/parts/LineTitle.vue
+++ src/components/parts/LineTitle.vue
@@ -1,10 +1,10 @@
 <template>
     <div>
-        <v-toolbar dark prominent src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg">
+        <v-toolbar prominent color="rgb(0, 195, 0,0.5)">
         <v-toolbar-title>おばあちゃん</v-toolbar-title>
         <v-spacer></v-spacer>
-        <v-btn class="ml-1" variant="text" icon="mdi-magnify"></v-btn>
-        <v-btn class="ml-1" variant="text" icon="mdi-phone" @click="click">
+        <v-btn class="ml-1" variant="text" icon="mdi-magnify" @click="click"></v-btn>
+        <v-btn class="ml-1" variant="text" icon="mdi-phone">
         </v-btn>
         <v-app-bar-nav-icon></v-app-bar-nav-icon>
         </v-toolbar>
@@ -13,6 +13,11 @@
     
 <script lang="ts">
     export default {
-    name: 'LineTitle'
+    name: 'LineTitle',
+    methods:{
+        click: function():void{
+            this.$emit("click");
+        },
+    }
     }
 </script>
