29fbb7ce74791a5e6ac415852fb01812bb428ffa
KessaPassa <k.someya.gyao@gmail.com>
2022-09-10 19:03:53 +0900

    run: npm run lint

--- frontend/src/components/parts/MainHeader.vue
+++ frontend/src/components/parts/MainHeader.vue
@@ -1,5 +1,5 @@
 <template>
-<v-app-bar color="primary" density="compact">
+  <v-app-bar color="primary" density="compact">
     <template v-slot:prepend>
       <v-app-bar-nav-icon color="white">≡</v-app-bar-nav-icon>
     </template>
@@ -7,11 +7,11 @@
     <template v-slot:append>
       <v-btn icon="mdi-dots-vertical"></v-btn>
     </template>
-  </v-app-bar>  
-</template>  
+  </v-app-bar>
+</template>
 
 <script lang="ts">
-  export default {
-    name: 'MainHeader'
-  }
+export default {
+  name: "MainHeader",
+};
 </script>
