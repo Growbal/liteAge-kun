5af664732abf4c59dd7fc2c49405b78d83f2053d
KessaPassa <k.someya.gyao@gmail.com>
2022-09-23 08:59:35 +0900

    [fix] 認証は専用のテーブルに切り出したので

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
+  mount_devise_token_auth_for 'UserAuthentication', at: 'auth'
   namespace :api do
     resources :users, only: %i[] do
       member do
