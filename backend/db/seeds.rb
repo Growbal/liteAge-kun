4787b0bfe90d5dd32ee2fccd7eb7837706ba96c7
KessaPassa <k.someya.gyao@gmail.com>
2022-09-23 10:05:43 +0900

    [add] seedデータを作成した

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,8 +1,24 @@
 # frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+ActiveRecord::Base.transaction(joinable: false, requires_new: true) do
+  question = Question.create!(
+    id: 1,
+    description: 'LINEおばあちゃん問題',
+  )
+
+  (1..5).each do |user_index|
+    user = User.create!(
+      id: user_index,
+      name: "かず#{user_index}",
+    )
+
+    (1..3).each do |waypoint_index|
+      UserScore.create!(
+        user:,
+        question:,
+        waypoint_status: waypoint_index,
+        score: SecureRandom.random_number(100),
+      )
+    end
+  end
+end
